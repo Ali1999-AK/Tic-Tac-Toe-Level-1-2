fc95a3de615a183e27376bf6d51465c3aa1e757f
Ali1999-AK <63508161+Ali1999-AK@users.noreply.github.com>
2020-07-01 14:09:18 +0530

    Add files via upload
    Updated

--- Tic_tack.py
+++ Tic_tack.py
@@ -1,4 +1,4 @@
-import IPython.display as ip
+#import IPython.display as ip
 import random
 class Board():
     def __init__(self):
@@ -18,7 +18,7 @@ class Board():
     def winner(self,plyr):
         return ((self.cells[1] == self.cells[2] == self.cells[3] == plyr) or (self.cells[4] == self.cells[5] == self.cells[6] == plyr)
                or (self.cells[7] == self.cells[8] == self.cells[9] == plyr) or
-               (self.cells[7] == self.cells[5] == self.cells[3] == plyr) or
+               (self.cells[7] == self.cells[5] == self.cells[8] == plyr) or
                (self.cells[1] == self.cells[5] == self.cells[9] == plyr))
     def reset(self):
         self.cells = [' ' for i in range(0,10)]
@@ -36,19 +36,26 @@ class Board():
             if self.cells[r] == ' ':
                 return r
     def print_head(self):
-        ip.clear_output()
+        #ip.clear_output()
         print('Welcome to Tic tac toe')
         self.reset()
         self.display()
-def main():
-    board.print_head()
-    while True:
-        
+    @staticmethod
+    def choice_player():
+        print('0 - 2 bots \n 1- player vs computer \n 2 - player vs player')
+        choice = input("Enter the no of player ")
+        while choice not in ['0','1','2']:
+            print('Enter valid option from 0,1,2')
+            choice = input("Enter the no of player ")
+        return int(choice)
+
+def two_players():
+    while True: 
         x_choice = int(input("Enter From  1 to 9 For X :"))
         while board.cells[x_choice] != " " :
             print(f'{x_choice} is full try other number')
             x_choice = int(input("Enter From  1 to 9 For X :"))
-        board.update_cell(x_choice,'X')#' * * \n  * \n * * '
+        board.update_cell(x_choice,'X')
         board.display()
         if board.winner('X'):
             print("X is a Winner")
@@ -65,9 +72,12 @@ def main():
                 board.reset()
                 continue
             else:
-                break    
-        o_choice = board.ai_move('O')
-        board.update_cell(o_choice,'O')#' * * \n  * \n * * '
+                break
+        o_choice = int(input("Enter From  1 to 9 For O :"))
+        while board.cells[o_choice] != " " :
+            print(f'{o_choice} is full try other number')
+            o_choice = int(input("Enter From  1 to 9 For O :"))
+        board.update_cell(o_choice,'O')
         board.display()
         if board.winner('O'):
             print("O is a Winner")
@@ -85,5 +95,107 @@ def main():
                 continue
             else:
                 break
+
+def one_players():
+    selct_ltr = input("Enter the letter X or O ").upper()
+    while selct_ltr != 'X' and selct_ltr != 'O':
+        print("Enter only X or O n No other Letters ")
+        selct_ltr = input("Enter the letter X or O ").upper()
+    if selct_ltr == 'X':
+        while True:
+            x_choice = int(input("Enter From  1 to 9 For X : "))
+            while board.cells[x_choice] != " " :
+                print(f'{x_choice} is full try other number')
+                x_choice = int(input("Enter From  1 to 9 For X : "))
+            board.update_cell(x_choice,'X')#' * * \n  * \n * * '
+            board.display()
+            if board.winner('X'):
+                print("X is a Winner")
+                play_again = input("Would You like to Play again? (Y/N) :").upper()
+                if play_again == 'Y':
+                    board.reset()
+                    continue
+                else:
+                    break
+            if board.is_tie():
+                print("Game is Tie")
+                play_again = input("Would You like to Play again? (Y/N) :").upper()
+                if play_again == 'Y':
+                    board.reset()
+                    continue
+                else:
+                    break    
+            o_choice = board.ai_move('O')
+            board.update_cell(o_choice,'O')#' * * \n  * \n * * '
+            board.display()
+            if board.winner('O'):
+                print("O is a Winner")
+                play_again = input("Would You like to Play again? (Y/N) :").upper()
+                if play_again == 'Y':
+                    board.reset()
+                    continue
+                else:
+                    break
+            if board.is_tie():
+                print("Game is Tie")
+                play_again = input("Would You like to Play again? (Y/N) :").upper()
+                if play_again == 'Y':
+                    board.reset()
+                    continue
+                else:
+                    break
+
+    if selct_ltr == 'O':
+        while True:
+            x_choice = board.ai_move('X') 
+            while board.cells[x_choice] != " " :
+                print(f'{x_choice} is full try other number')
+                x_choice = int(input("Enter From  1 to 9 For X :"))
+            board.update_cell(x_choice,'X')#' * * \n  * \n * * '
+            board.display()
+            if board.winner('X'):
+                print("X is a Winner")
+                play_again = input("Would You like to Play again? (Y/N) :").upper()
+                if play_again == 'Y':
+                    board.reset()
+                    continue
+                else:
+                    break
+            if board.is_tie():
+                print("Game is Tie")
+                play_again = input("Would You like to Play again? (Y/N) :").upper()
+                if play_again == 'Y':
+                    board.reset()
+                    continue
+                else:
+                    break    
+            o_choice = int(input("Enter From  1 to 9 For O :"))
+            board.update_cell(o_choice,'O')#' * * \n  * \n * * '
+            board.display()
+            if board.winner('O'):
+                print("O is a Winner")
+                play_again = input("Would You like to Play again? (Y/N) :").upper()
+                if play_again == 'Y':
+                    board.reset()
+                    continue
+                else:
+                    break
+            if board.is_tie():
+                print("Game is Tie")
+                play_again = input("Would You like to Play again? (Y/N) :").upper()
+                if play_again == 'Y':
+                    board.reset()
+                    continue
+                else:
+                    break
+
+def main():
+    board.print_head()
+    c = board.choice_player()
+    if c == 2:
+        two_players()
+    if c == 1:
+        one_players()
+
 board = Board()
 main()
